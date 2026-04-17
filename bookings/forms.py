21fdb6ad79c9fd96de1ce357217c201b9c36d78f
ravishsonia404 <ravishsonia404@gmail.com>
2026-04-17 11:14:52 +0530

    Update forms.py

--- bookings/forms.py
+++ bookings/forms.py
@@ -4,4 +4,31 @@ from .models import Ride
 class RideForm(forms.ModelForm):
     class Meta:
         model = Ride
-        fields = '__all__'
+        fields = ['name', 'email', 'pickup', 'drop', 'date', 'time']
+
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'form-control form-control-lg',
+                'placeholder': 'Enter your name'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control form-control-lg',
+                'placeholder': 'Enter your email'
+            }),
+            'pickup': forms.TextInput(attrs={
+                'class': 'form-control form-control-lg',
+                'placeholder': 'Pickup location'
+            }),
+            'drop': forms.TextInput(attrs={
+                'class': 'form-control form-control-lg',
+                'placeholder': 'Drop location'
+            }),
+            'date': forms.DateInput(attrs={
+                'class': 'form-control form-control-lg',
+                'type': 'date'
+            }),
+            'time': forms.TimeInput(attrs={
+                'class': 'form-control form-control-lg',
+                'type': 'time'
+            }),
+        }
